cbaa6edbb006d22d2e00d15667a48eef162c975f
Rafael Alcides <rafalcides@icloud.com>
2022-08-08 22:26:10 -0300

    Media Queries da tela

--- lib/src/auth/sing_in_screen.dart
+++ lib/src/auth/sing_in_screen.dart
@@ -8,133 +8,140 @@ class SingInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
-      body: Column(
-        children: [
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:  [
-                Text.rich(
-                  TextSpan(
-                    style: const TextStyle(
-                      fontSize: 40
-                    ),
-                    children: [
-                      const TextSpan(
-                        text: "Loja do ",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold
-                        )
-                      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:  [
+                    Text.rich(
                       TextSpan(
-                        text: "Menor",
-                        style: TextStyle(
-                          color: CustomColors.customContrastColor,
-                          fontWeight: FontWeight.bold
+                        style: const TextStyle(
+                          fontSize: 40
+                        ),
+                        children: [
+                          const TextSpan(
+                            text: "Loja do ",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold
+                            )
+                          ),
+                          TextSpan(
+                            text: "Menor",
+                            style: TextStyle(
+                              color: CustomColors.customContrastColor,
+                              fontWeight: FontWeight.bold
+                            )
+                          )
+                        ]
                         )
-                      )
-                    ]
+                    ),
+                    
+                    SizedBox(
+                      height: 30,
+                      child: DefaultTextStyle(
+                        style: const TextStyle(
+                          fontSize: 25,
+                        ),
+                        child: AnimatedTextKit(
+                          repeatForever: true,
+                          pause: Duration.zero,
+                          animatedTexts: [
+                            FadeAnimatedText('Olha eu!'),
+                            FadeAnimatedText('Olha eu Chegando!'),
+                            FadeAnimatedText('Venha ver!'),
+                            FadeAnimatedText('Passando em sua porta!'),
+                            FadeAnimatedText('Tempero do Menor'),
+                          ]
+                        ),
+                      ),
                     )
+                  ],
+                ) 
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(45)
+                  )
                 ),
-                
-                SizedBox(
-                  height: 30,
-                  child: DefaultTextStyle(
-                    style: const TextStyle(
-                      fontSize: 25,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const CustomTextFild(
+                      icon: Icons.email,
+                      label: "E-mail",
                     ),
-                    child: AnimatedTextKit(
-                      repeatForever: true,
-                      pause: Duration.zero,
-                      animatedTexts: [
-                        FadeAnimatedText('Olha eu!'),
-                        FadeAnimatedText('Olha eu Chegando!'),
-                        FadeAnimatedText('Venha ver!'),
-                        FadeAnimatedText('Passando em sua porta!'),
-                        FadeAnimatedText('Tempero do Menor'),
-                      ]
+                    const CustomTextFild(
+                      icon: Icons.lock,
+                      label: "Senha",
+                      isSecret: true,
                     ),
-                  ),
-                )
-              ],
-            ) 
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 20),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(45)
-              )
-            ),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const CustomTextFild(
-                    icon: Icons.email,
-                    label: "E-mail",
-                  ),
-                  const CustomTextFild(
-                    icon: Icons.lock,
-                    label: "Senha",
-                    isSecret: true,
-                  ),
-                  SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(45)
+                    SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(45)
+                          )
+                        ),
+                        onPressed: () {}, 
+                        child: const Text("Entrar")
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {}, 
+                        child: Text("Esqueceu a senha",
+                        style: TextStyle(
+                          color: CustomColors.customContrastColor
+                        ),
                         )
                       ),
-                      onPressed: () {}, 
-                      child: const Text("Entrar")
                     ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {}, 
-                      child: Text("Esqueceu a senha",
-                      style: TextStyle(
-                        color: CustomColors.customContrastColor
-                      ),
-                      )
+                    Row(
+                      children: const [
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey,
+                            thickness: 2,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: Text("Ou"),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey,
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  Row(
-                    children: const [
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey,
-                          thickness: 2,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Text("Ou"),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          color: Colors.grey,
-                          thickness: 2,
-                        ),
-                      ),
-                    ],
-                  ),
-                  TextButton(
-                    onPressed: () {}, 
-                    child: const Text("Criar conta Agora")
-                  )
-                ],
+                    TextButton(
+                      onPressed: () {}, 
+                      child: const Text("Criar conta Agora")
+                    )
+                  ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
